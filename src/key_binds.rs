dea3ff241d34c93d5ab37ec80459b82c1fea2d09
Vadim Khitrin <me@vkhitrin.com>
2024-10-18 16:42:10 +0300

    ci: Adding Initial CI
    Addingw workflows:
    * `lint` lints project.
    * `build` builds project.

--- src/key_binds.rs
+++ src/key_binds.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
+use cosmic::iced::keyboard::Key;
 use cosmic::widget::menu::key_bind::KeyBind;
 use cosmic::widget::menu::key_bind::Modifier;
-use cosmic::iced::keyboard::Key;
 
 use crate::app::MenuAction;
 
